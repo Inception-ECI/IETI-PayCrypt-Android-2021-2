bdffd5a44e170e335be330a163656ff81c0d3ba0
Andrés Calderón <mateo.cor.amco@gmail.com>
2021-12-02 17:22:42 -0500

    Initial Paycrypt Android

---
+++ README.md
@@ -0,0 +1,10 @@
+## Inception - AndroidProject - PayCrypt 💱💲
+
+### Integrantes ✒️
+
+- Laura Alejandra Bernal Hernández - [lale1507](https://github.com/lale1507)
+- Juan Sebastian Ramos Isaza - [jsr25](https://github.com/Candres1019)
+- Andres Mateo Calderon Ortega - [Candres1019](https://github.com/Candres1019)
+- Paula Andrea Guevara Sánchez - [PaulaSanchez810](https://github.com/PaulaSanchez810)
+- Daniel Felipe Rincon Muñoz - [danielrincon-m](https://github.com/danielrincon-m)
+- Guillermo Alfonso Castro Camelo - [memo1019](https://github.com/memo1019)
---
+++ app/src/main/java/com/inception/paycrypt/MainActivity.java
@@ -0,0 +1,19 @@
+package com.inception.paycrypt;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(view -> {
+            TextView textView = findViewById(R.id.textView);
+            textView.setText("Welcome to paycrypt");
+        });
+    }
+}
